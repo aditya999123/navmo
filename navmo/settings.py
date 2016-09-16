b29e063a228333d1ed0fdea0c51a073b6bfbeb6b
Meghal <meghal.nit@gmail.com>
2016-09-16 16:53:49 +0530

    Changing sqlite to mysql

--- navmo/settings.py
+++ navmo/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'navmo.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-}
+#}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
