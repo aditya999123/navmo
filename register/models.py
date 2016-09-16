9d31e15fe413952d9b1a8df78a7a00600ef0db96
Aditya <aditya999123@gmail.com>
2016-09-16 08:49:45 +0530

    bugs fixed

--- register/models.py
+++ register/models.py
@@ -18,14 +18,14 @@ class user_data(models.Model):
     gender=models.CharField(max_length=120,blank=True,null=True)
     exam_centre_1=models.CharField(max_length=120,blank=True,null=True)
     exam_centre_2=models.CharField(max_length=120,blank=True,null=True)
-    flag_exam_group_1=models.PositiveSmallIntegerField(default=0)
-    flag_exam_group_2=models.PositiveSmallIntegerField(default=0)
-    flag_workshop=models.PositiveSmallIntegerField(default=0)
-    flag_mpe_student=models.PositiveSmallIntegerField(default=0)
     exam_group_1=models.CharField(max_length=120,blank=True,null=True)
     exam_group_2=models.CharField(max_length=120,blank=True,null=True)
     flag_exam_centre_1=models.PositiveSmallIntegerField(default=0)
     flag_exam_centre_2=models.PositiveSmallIntegerField(default=0)
+    flag_exam_group_1=models.PositiveSmallIntegerField(default=0)
+    flag_exam_group_2=models.PositiveSmallIntegerField(default=0)
+    flag_workshop=models.PositiveSmallIntegerField(default=0)
+    flag_mpe_student=models.PositiveSmallIntegerField(default=0)
     image=models.ImageField(upload_to='/media/user_images/',default="/media/default.png")
 
 class exam_center_data(models.Model):
