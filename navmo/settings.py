f6181a4e8bebf63fb1713b0c11715474aab4f0fb
Meghal <meghal.nit@gmail.com>
2016-09-16 16:52:11 +0530

    Changing sqlite to mysql

--- navmo/settings.py
+++ navmo/settings.py
@@ -72,22 +72,22 @@ WSGI_APPLICATION = 'navmo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'navmo',
-#         'USER': 'root',
-#         'PASSWORD': 'Localcart@999123',
-#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-# }
+}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'navmo',
+        'USER': 'root',
+        'PASSWORD': 'Localcart@999123',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
